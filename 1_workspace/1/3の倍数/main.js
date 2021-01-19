31de9b54a7896427a461c14549f757250c6f4ff4
小林　華 <kobayashihana@kobayashihananoMacBook-Air.local>
2021-01-20 02:46:24 +0900

    テスト

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,9 +1,17 @@
-const love3Function = function () {
-for (let n = 1;n <100; n++){
-    console.log(n);
+const genkifunction = function(number){
+for (let n=1; n<=number; n++) {
+     if (n % 15 ===0){
+        console.log ("fizzbuzz")
+}
+else if (n % 3 === 0 ) {
+    console.log ("fizz")}
+
+else if (n % 5 ===0){
+    console.log ("buzz")}
+else {
+    console.log (n)
 }
 
 }
-love3Function()
-
-
+}
+genkifunction (100)
